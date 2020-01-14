22246e62950f664e02a461dc1ae504c4a32040af
Danika Thomson <danikathomson@Danikas-mac.local>
2020-01-14 14:38:58 +0100

    added mouseover event to logo heading

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+const logoChange = document.querySelector(".logo-heading");
+console.log(logoChange);
+
+logoChange.addEventListener("mouseover", e => {
+console.log("I'm under mouse attack!")
+logoChange.style.color = "#DC143C";
+logoChange.style.fontSize = "60px";
+});
+
